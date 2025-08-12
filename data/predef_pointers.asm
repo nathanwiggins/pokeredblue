0b8ee76b9cb9ddf849936bf5f3ae7f9a89122d63
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-12 06:36:15 -0600

    Add PokedexToIndex predef

--- data/predef_pointers.asm
+++ data/predef_pointers.asm
@@ -67,9 +67,10 @@ PredefPointers::
 	add_predef StatusScreen
 	add_predef StatusScreen2
 	add_predef InternalClockTradeAnim
-	add_predef TrainerEngage
-	add_predef IndexToPokedex
-	add_predef DisplayPicCenteredOrUpperRight
+        add_predef TrainerEngage
+        add_predef IndexToPokedex
+        add_predef PokedexToIndex
+        add_predef DisplayPicCenteredOrUpperRight
 	add_predef UsedCut
 	add_predef ShowPokedexData
 	add_predef WriteMonMoves
