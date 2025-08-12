f5caf599936303a1ddf6ea50d6e98158543dd685
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-12 12:16:42 -0400

    Update overworld.asm

--- home/overworld.asm
+++ home/overworld.asm
@@ -494,7 +494,7 @@ WarpFound2::
 	ld [wUnusedLastMapWidth], a
 	ldh a, [hWarpDestinationMap]
 	ld [wCurMap], a
-	cp MT_EMBER_2F
+	cp MT_EMBER_1F
 	jr z, .needsFlash
 	cp ROCK_TUNNEL_1F
 	jr nz, .notRockTunnel
