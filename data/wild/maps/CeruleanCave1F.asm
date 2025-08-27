9461c40e2d43ad38d249576bff78d4b2e4011dea
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-27 15:54:02 -0600

    Update CeruleanCave1F.asm

--- data/wild/maps/CeruleanCave1F.asm
+++ data/wild/maps/CeruleanCave1F.asm
@@ -1,21 +1,46 @@
 CeruleanCave1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 46, GOLBAT
-	db 46, HYPNO
-	db 46, MAGNETON
-	db 49, DODRIO
-	db 49, VENOMOTH
 IF DEF(_RED)
-	db 52, ARBOK
+	db  50, ALAKAZAM
+	db  50, ALAKAZAM
+	db  50, GENGAR
+	db  50, GENGAR
+	db  60, NIDOKING
+	db  60, NIDOKING
+	db  60, NIDOQUEEN
+	db  60, AERODACTYL
+	db  65, DRAGONITE
+	db  65, DRAGONITE
 ENDC
 IF DEF(_BLUE)
-	db 52, SANDSLASH
+	db  50, ALAKAZAM
+	db  50, ALAKAZAM
+	db  50, GENGAR
+	db  50, GENGAR
+	db  60, NIDOQUEEN
+	db  60, NIDOQUEEN
+	db  60, NIDOKING
+	db  60, AERODACTYL
+	db  65, DRAGONITE
+	db  65, DRAGONITE
 ENDC
-	db 49, KADABRA
-	db 52, PARASECT
-	db 53, RAICHU
-	db 53, DITTO
 	end_grass_wildmons
+
+; Red Version:
+; ALAKAZAM (30%)
+; GENGAR (30%)
+; NIDOKING (20%)
+; NIDOQUEEN (10%)
+; AERODACTYL (5%)
+; DRAGONITE (5%)
+
+; Blue Version:
+; ALAKAZAM (30%)
+; GENGAR (30%)
+; NIDOQUEEN (20%)
+; NIDOKING (10%)
+; AERODACTYL (5%)
+; DRAGONITE (5%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
