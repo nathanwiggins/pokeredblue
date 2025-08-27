2a630ebb99520b12f2690cba48b7549870aa4be3
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-27 16:36:07 -0600

    Update VictoryRoad2F.asm

--- data/wild/maps/VictoryRoad2F.asm
+++ data/wild/maps/VictoryRoad2F.asm
@@ -1,16 +1,25 @@
 VictoryRoad2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 22, MACHOP
-	db 24, GEODUDE
-	db 26, ZUBAT
-	db 36, ONIX
-	db 39, ONIX
-	db 42, ONIX
-	db 41, MACHOKE
-	db 40, GOLBAT
-	db 40, MAROWAK
-	db 43, GRAVELER
+	db  45, DUGTRIO
+	db  45, MAROWAK
+	db  50, MACHAMP
+	db  50, GOLEM
+	db  50, MACHAMP
+	db  50, GOLEM
+	db  45, RHYDON
+	db  60, OMASTAR
+	db  60, KABUTOPS
+	db  60, KABUTOPS
 	end_grass_wildmons
+
+; Red & Blue Version:
+; DUGTRIO (15%)
+; MAROWAK (15%)
+; MACHAMP (25%)
+; GOLEM (25%)
+; RHYDON (10%)
+; OMASTAR (5%)
+; KABUTOPS (5%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
