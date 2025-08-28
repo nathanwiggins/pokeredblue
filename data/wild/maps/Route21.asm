f8e9a6f1a1812ae856c38343e105d1fe2ff525ef
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-28 16:19:57 -0600

    Update Route21.asm

--- data/wild/maps/Route21.asm
+++ data/wild/maps/Route21.asm
@@ -1,16 +1,22 @@
 Route21WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 21, RATTATA
-	db 23, PIDGEY
-	db 30, RATICATE
-	db 23, RATTATA
-	db 21, PIDGEY
-	db 30, PIDGEOTTO
-	db 32, PIDGEOTTO
-	db 28, TANGELA
-	db 30, TANGELA
-	db 32, TANGELA
+	db  45, PARASECT
+	db  45, PARASECT
+	db  45, PARASECT
+	db  45, PERSIAN
+	db  45, PERSIAN
+	db  45, PERSIAN
+	db  45, TANGELA
+	db  45, TANGELA
+	db  30, IVYSAUR
+	db  30, IVYSAUR
 	end_grass_wildmons
+
+; Red & Blue Version:
+; PARASECT (45%)
+; PERSIAN (35%)
+; TANGELA (15%)
+; IVYSAUR (5%)
 
 	def_water_wildmons 5 ; encounter rate
 	db  5, TENTACOOL
