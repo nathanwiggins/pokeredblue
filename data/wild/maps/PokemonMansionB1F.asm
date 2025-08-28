c0a320b7253dd8037371b7cbdcae6486f8ad0879
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-28 16:27:22 -0600

    Update PokemonMansionB1F.asm

--- data/wild/maps/PokemonMansionB1F.asm
+++ data/wild/maps/PokemonMansionB1F.asm
@@ -1,30 +1,24 @@
 PokemonMansionB1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-IF DEF(_RED)
-	db 33, KOFFING
-	db 31, KOFFING
-	db 35, GROWLITHE
-	db 32, PONYTA
-	db 31, KOFFING
-	db 40, WEEZING
-	db 34, PONYTA
-	db 35, GRIMER
-	db 42, WEEZING
-	db 42, MUK
-ENDC
-IF DEF(_BLUE)
-	db 33, GRIMER
-	db 31, GRIMER
-	db 35, VULPIX
-	db 32, PONYTA
-	db 31, GRIMER
-	db 40, MUK
-	db 34, PONYTA
-	db 35, KOFFING
-	db 38, MAGMAR
-	db 42, WEEZING
-ENDC
+	db  45, MUK
+	db  45, MUK
+	db  45, WEEZING
+	db  45, WEEZING
+	db  45, HYPNO
+	db  45, HYPNO
+	db  45, MR_MIME
+	db  50, HITMONLEE
+	db  50, HITMONCHAN
+	db  50, HITMONCHAN
 	end_grass_wildmons
+
+; Red & Blue Version:
+; MUK (30%)
+; WEEZING (30%)
+; HYPNO (20%)
+; MR_MIME (10%)
+; HITMONLEE (5%)
+; HITMONCHAN (5%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
