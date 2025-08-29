f80fe4bdb282557ed054c5e00ef5b4f0a18ae3bc
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-29 14:27:33 -0600

    Update Route15.asm

--- data/wild/maps/Route15.asm
+++ data/wild/maps/Route15.asm
@@ -1,28 +1,42 @@
 Route15WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 24, ODDISH
-	db 26, DITTO
-	db 23, PIDGEY
-	db 26, VENONAT
-	db 22, ODDISH
-	db 28, VENONAT
-	db 26, ODDISH
-	db 30, GLOOM
+	db  45, BUTTERFREE
+	db  45, BEEDRILL
+	db  45, BEEDRILL
+	db  45, BEEDRILL
+	db  45, VENOMOTH
+	db  45, VENOMOTH
+	db  45, VENOMOTH
+	db  45, CHANSEY
+	db  45, CHANSEY
+	db  45, CHANSEY
 ENDC
 IF DEF(_BLUE)
-	db 24, BELLSPROUT
-	db 26, DITTO
-	db 23, PIDGEY
-	db 26, VENONAT
-	db 22, BELLSPROUT
-	db 28, VENONAT
-	db 26, BELLSPROUT
-	db 30, WEEPINBELL
+	db  45, BUTTERFREE
+	db  45, BUTTERFREE
+	db  45, BUTTERFREE
+	db  45, BEEDRILL
+	db  45, VENOMOTH
+	db  45, VENOMOTH
+	db  45, VENOMOTH
+	db  45, CHANSEY
+	db  45, CHANSEY
+	db  45, CHANSEY
 ENDC
-	db 28, PIDGEOTTO
-	db 30, PIDGEOTTO
 	end_grass_wildmons
+
+; Red Version:
+; BUTTERFREE (15%)
+; BEEDRILL (45%)
+; VENOMOTH (30%)
+; CHANSEY (10%)
+
+; Blue Version:
+; BUTTERFREE (45%)
+; BEEDRILL (15%)
+; VENOMOTH (30%)
+; CHANSEY (10%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
