92e1c55bf041cce92feb03ef42772adf35307bdf
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-29 14:36:08 -0600

    Update Route16.asm

--- data/wild/maps/Route16.asm
+++ data/wild/maps/Route16.asm
@@ -1,16 +1,46 @@
 Route16WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 18, RATTATA
-	db 20, DODUO
-	db 20, RATTATA
-	db 18, DODUO
-	db 22, DODUO
-	db 22, RATTATA
-	db 23, RATICATE
-	db 25, RATICATE
+IF DEF(_RED)
+	db  45, RATICATE
+	db  45, RATICATE
+	db  45, FEAROW
+	db  45, FEAROW
+	db  45, DODRIO
+	db  45, DODRIO
+	db  45, KANGASKHAN
+	db  45, SNORLAX
+	db  45, TAUROS
+	db  45, TAUROS
+ENDC
+IF DEF(_BLUE)
+	db  45, RATICATE
+	db  45, RATICATE
+	db  45, FEAROW
+	db  45, FEAROW
+	db  45, DODRIO
+	db  45, DODRIO
+	db  45, TAUROS
+	db  45, SNORLAX
+	db  45, KANGASKHAN
+	db  45, KANGASKHAN
+ENDC
 	end_grass_wildmons
+
+; Red Version:
+; RATICATE (30%)
+; FEAROW (30%)
+; DODRIO (20%)
+; KANGASKHAN (10%)
+; SNORLAX (5%)
+; TAUROS (5%)
+
+; Blue Version:
+; RATICATE (30%)
+; FEAROW (30%)
+; DODRIO (20%)
+; TAUROS (10%)
+; SNORLAX (5%)
+; KANGASKHAN (5%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
