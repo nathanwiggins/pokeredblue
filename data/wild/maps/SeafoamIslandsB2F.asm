008a2f3677fbe7eca97db1ee12a003c3e8080c94
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-08-29 14:18:11 -0600

    Update SeafoamIslandsB2F.asm

--- data/wild/maps/SeafoamIslandsB2F.asm
+++ data/wild/maps/SeafoamIslandsB2F.asm
@@ -1,29 +1,48 @@
 SeafoamIslandsB2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 30, SEEL
 IF DEF(_RED)
-	db 30, SLOWPOKE
-	db 32, SEEL
-	db 32, SLOWPOKE
-	db 28, HORSEA
-	db 30, STARYU
-	db 30, HORSEA
-	db 28, SHELLDER
-	db 30, GOLBAT
-	db 37, SLOWBRO
+	db  45, GOLDUCK
+	db  45, SLOWBRO
+	db  45, SLOWBRO
+	db  45, SLOWBRO
+	db  45, JYNX
+	db  45, DEWGONG
+	db  45, KINGLER
+	db  60, CLOYSTER
+	db  30, WARTORTLE
+	db  30, WARTORTLE
 ENDC
 IF DEF(_BLUE)
-	db 30, PSYDUCK
-	db 32, SEEL
-	db 32, PSYDUCK
-	db 28, KRABBY
-	db 30, SHELLDER
-	db 30, KRABBY
-	db 28, STARYU
-	db 30, GOLBAT
-	db 37, GOLDUCK
+	db  45, GOLDUCK
+	db  45, GOLDUCK
+	db  45, GOLDUCK
+	db  45, SLOWBRO
+	db  45, JYNX
+	db  45, DEWGONG
+	db  45, KINGLER
+	db  60, CLOYSTER
+	db  30, WARTORTLE
+	db  30, WARTORTLE
 ENDC
 	end_grass_wildmons
+
+; Red Version:
+; GOLDUCK (15%)
+; SLOWBRO (45%)
+; JYNX (10%)
+; DEWGONG (10%)
+; KINGLER (10%)
+; CLOYSTER (5%)
+; WARTORTLE (5%)
+
+; Blue Version:
+; GOLDUCK (45%)
+; SLOWBRO (15%)
+; JYNX (10%)
+; DEWGONG (10%)
+; KINGLER (10%)
+; CLOYSTER (5%)
+; WARTORTLE (5%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
