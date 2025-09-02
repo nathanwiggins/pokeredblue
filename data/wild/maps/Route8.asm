8194752966bb4439633c272d09dc2e175a7f79f7
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-02 13:41:53 -0600

    Update Route8.asm

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -1,29 +1,40 @@
 Route8WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 18, PIDGEY
 IF DEF(_RED)
-	db 18, MANKEY
-	db 17, EKANS
-	db 16, GROWLITHE
-	db 20, PIDGEY
-	db 20, MANKEY
-	db 19, EKANS
-	db 17, GROWLITHE
-	db 15, GROWLITHE
-	db 18, GROWLITHE
+	db  20, GROWLITHE
+	db  20, GROWLITHE
+	db  20, GROWLITHE
+	db  20, GROWLITHE
+	db  20, GROWLITHE
+	db  20, VULPIX
+	db  20, VULPIX
+	db  20, VULPIX
+	db  25, FARFETCHD
+	db  25, FARFETCHD
 ENDC
 IF DEF(_BLUE)
-	db 18, MEOWTH
-	db 17, SANDSHREW
-	db 16, VULPIX
-	db 20, PIDGEY
-	db 20, MEOWTH
-	db 19, SANDSHREW
-	db 17, VULPIX
-	db 15, VULPIX
-	db 18, VULPIX
+	db  20, VULPIX
+	db  20, VULPIX
+	db  20, VULPIX
+	db  20, VULPIX
+	db  20, VULPIX
+	db  20, GROWLITHE
+	db  20, GROWLITHE
+	db  20, GROWLITHE
+	db  25, FARFETCHD
+	db  25, FARFETCHD
 ENDC
 	end_grass_wildmons
+
+; Red Version:
+; GROWLITHE (70%)
+; VULPIX (25%)
+; FARFETCHD (5%)
+
+; Blue Version:
+; VULPIX (70%)
+; GROWLITHE (25%)
+; FARFETCHD (5%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
