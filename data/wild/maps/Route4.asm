307511e51ac9313369d45b3125900778ab7db1ad
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-02 14:00:26 -0600

    Update Route4.asm

--- data/wild/maps/Route4.asm
+++ data/wild/maps/Route4.asm
@@ -1,27 +1,42 @@
 Route4WildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db 10, RATTATA
-	db 10, SPEAROW
-	db  8, RATTATA
 IF DEF(_RED)
-	db  6, EKANS
-	db  8, SPEAROW
-	db 10, EKANS
-	db 12, RATTATA
-	db 12, SPEAROW
-	db  8, EKANS
-	db 12, EKANS
+	db  10, EKANS
+	db  10, EKANS
+	db  10, EKANS
+	db  10, SANDSHREW
+	db  10, GRIMER
+	db  10, GRIMER
+	db  10, GRIMER
+	db  10, KOFFING
+	db  10, KOFFING
+	db  10, KOFFING
 ENDC
 IF DEF(_BLUE)
-	db  6, SANDSHREW
-	db  8, SPEAROW
-	db 10, SANDSHREW
-	db 12, RATTATA
-	db 12, SPEAROW
-	db  8, SANDSHREW
-	db 12, SANDSHREW
+	db  10, EKANS
+	db  10, SANDSHREW
+	db  10, SANDSHREW
+	db  10, SANDSHREW
+	db  10, GRIMER
+	db  10, KOFFING
+	db  10, KOFFING
+	db  10, KOFFING
+	db  10, KOFFING
+	db  10, KOFFING
 ENDC
 	end_grass_wildmons
+
+; Red Version:
+; EKANS (45%)
+; SANDSHREW (15%)
+; GRIMER (30%)
+; KOFFING (10%)
+
+; Blue Version:
+; EKANS (15%)
+; SANDSHREW (45%)
+; GRIMER (10%)
+; KOFFING (30%)
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
