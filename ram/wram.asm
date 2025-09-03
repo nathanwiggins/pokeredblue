39be07309425ed61a941709b01ae2dfff7cfc49b
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-03 10:45:23 -0600

    Update wram.asm

--- ram/wram.asm
+++ ram/wram.asm
@@ -2037,7 +2037,9 @@ wRoute18Gate1FCurScript:: db
 	ds 78
 wGameProgressFlagsEnd::
 
-	ds 56
+wRandomWildMonClasses:: ds NUM_RANDOM_CLASSES
+
+	ds 53
 
 wObtainedHiddenItemsFlags:: flag_array MAX_HIDDEN_ITEMS
 
@@ -2192,9 +2194,7 @@ wOpponentAfterWrongAnswer:: db
 ; mostly copied from map-specific map script pointer and written back later
 wCurMapScript:: db
 
-wRandomWildMonClasses:: ds NUM_RANDOM_CLASSES
-
-	ds 4
+	ds 7
 
 wPlayTimeHours:: db
 wPlayTimeMaxed:: db
