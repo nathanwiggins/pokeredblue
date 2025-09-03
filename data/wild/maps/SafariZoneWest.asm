8675ebabffdd7486ee428b71c6716f80739376fe
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-03 15:57:23 -0600

    Update SafariZoneWest.asm

--- data/wild/maps/SafariZoneWest.asm
+++ data/wild/maps/SafariZoneWest.asm
@@ -1,29 +1,28 @@
 SafariZoneWestWildMons:
 	def_grass_wildmons 30 ; encounter rate
-IF DEF(_RED)
-	db 25, NIDORAN_M
-	db 26, DODUO
-	db 23, VENONAT
-	db 24, EXEGGCUTE
-	db 33, NIDORINO
-	db 26, EXEGGCUTE
-	db 25, NIDORAN_F
-ENDC
-IF DEF(_BLUE)
-	db 25, NIDORAN_F
-	db 26, DODUO
-	db 23, VENONAT
-	db 24, EXEGGCUTE
-	db 33, NIDORINA
-	db 26, EXEGGCUTE
-	db 25, NIDORAN_M
-ENDC
-	db 31, VENOMOTH
-	db 26, TAUROS
-	db 28, KANGASKHAN
+	db 20, RANDOM_CLASS_8
+	db 20, RANDOM_CLASS_8
+	db 20, RANDOM_CLASS_8
+	db 30, RANDOM_CLASS_13
+	db 30, RANDOM_CLASS_13
+	db 40, RANDOM_CLASS_18
+	db 40, RANDOM_CLASS_18
+	db 50, RANDOM_CLASS_23
+	db 50, RANDOM_CLASS_23
+	db 50, RANDOM_CLASS_23
 	end_grass_wildmons
 
-	def_water_wildmons 0 ; encounter rate
+	def_water_wildmons 20 ; encounter rate
+	db 20, RANDOM_CLASS_4
+	db 20, RANDOM_CLASS_4
+	db 20, RANDOM_CLASS_4
+	db 30, RANDOM_CLASS_9
+	db 30, RANDOM_CLASS_9
+	db 40, RANDOM_CLASS_14
+	db 40, RANDOM_CLASS_14
+	db 50, RANDOM_CLASS_19
+	db 50, RANDOM_CLASS_19
+	db 50, RANDOM_CLASS_19
 	end_water_wildmons
 
 ;Land 8 13 18 23
