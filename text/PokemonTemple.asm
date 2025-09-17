ac79c7fd93f4c62704c622b73df54a833ef9ea5f
nathanwiggins <120141926+nathanwiggins@users.noreply.github.com>
2025-09-17 16:49:00 -0600

    Update PokemonTemple.asm

--- text/PokemonTemple.asm
+++ text/PokemonTemple.asm
@@ -3,3 +3,12 @@ _PokemonTempleMewBattleText::
 	  line "..."
 	  cont "Mew!@"
     text_end
+	
+
+_PokemonTemplePurifiedZoneText::
+    text "A bright aura"
+    line "surrounds you."
+
+    para "<PLAYER>'s #MON"
+    line "are fully healed!"
+    done
